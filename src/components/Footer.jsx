6b9a05b4fc5b5fac81205230b18d19d48a278534
Anush79 <barbieworld1056.a@gmail.com>
2024-01-21 19:33:15 +0530

    changed link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <div className="footer">
-      <p>&copy; 2024 FixHealth | Designed by <a href="https://github.com/anush79" target="_blank" rel="noopener noreferrer">Anushka Jaiswal</a></p>
+      <p>&copy; 2024 FixHealth | Developed by <a href="https://www.linkedin.com/in/anushka-jaiswal-46258918a/" target="_blank" rel="noopener noreferrer">Anushka Jaiswal</a></p>
     </div>
   );
 };
